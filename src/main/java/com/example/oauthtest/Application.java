01a3c89bcc0a3d8f9e9a7ec0e48c1c7ea19cdb4c
aidenshin <shin-ilhyun@hanmail.net>
2021-11-16 09:45:47 +0900

    Initial commit

---
+++ src/main/java/com/example/oauthtest/Application.java
@@ -0,0 +1,58 @@
+package com.example.oauthtest;
+
+import com.example.oauthtest.config.ShutDownCustom;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+import java.util.TimeZone;
+
+@ServletComponentScan
+@EnableJpaAuditing
+@EnableJpaRepositories
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.addListeners(new ApplicationPidFileWriter());   // pid 를 작성하는 역할을 하는 클래스 선언
+        app.run(args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        Locale.setDefault(Locale.KOREA);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ShutDownCustom shutdown() {
+        return new ShutDownCustom();
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(final ShutDownCustom shutDownCustom) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(shutDownCustom);
+        return factory;
+    }
+
+}
